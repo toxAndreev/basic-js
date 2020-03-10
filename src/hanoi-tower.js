b744edf4ff0605e43e73742b91caf5f80a0d6db2
toxAndreev <36535854+toxAndreev@users.noreply.github.com>
2020-03-10 21:17:35 +0200

    Update hanoi-tower.js

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,5 +1,21 @@
-module.exports = function calculateHanoi( disksNumber, turnsSpeed ) {
-    let turns = 2**disksNumber - 1;
-    let seconds = turns / (turnsSpeed / 3600);
-    return {  turns, seconds };
-}
+module.exports = class DepthCalculator {
+    maxDepth = 0;
+    currentLevel = 0;
+    calculateDepth(arr, isInternalRun) {
+        if (!isInternalRun){
+            this.maxDepth = 0;
+            this.currentLevel = 0;
+        } 
+        if (arr instanceof Array){
+            this.currentLevel += 1;
+            arr.forEach(x => {
+                if (x instanceof Array){
+                     this.calculateDepth(x, true);
+                }
+            });
+            if (this.currentLevel > this.maxDepth) this.maxDepth = this.currentLevel;
+            this.currentLevel -= 1;
+        }
+        return this.maxDepth;
+    }
+};
