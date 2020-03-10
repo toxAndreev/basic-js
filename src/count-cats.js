9053a35ddd98d26f5b6457cb60786e8c01f1f0bd
toxAndreev <36535854+toxAndreev@users.noreply.github.com>
2020-03-10 21:15:23 +0200

    Update count-cats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,16 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let catsCount = 0;
+  if (matrix && matrix instanceof Array){
+    matrix.forEach(row => {
+      if (row && row instanceof Array){
+        row.forEach(elemToCheck => {
+          if (elemToCheck 
+              && elemToCheck.toString() == '^^'){
+                ++catsCount;
+              }
+        })
+      }
+    });
+  }
+  return catsCount;
 };
