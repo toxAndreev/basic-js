c2d05faf1c5c87c2bc75e82b457e5f49e002bf12
toxAndreev <36535854+toxAndreev@users.noreply.github.com>
2020-03-10 21:19:03 +0200

    Update transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,21 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform( arr ) {
+    if (!(arr instanceof Array)) throw new Error();
+    let retValue = [];
+    for (let i=0; i < arr.length; ++i){
+        if (arr[i] == '--discard-next') ++i;
+        else if (arr[i] == '--discard-prev'){
+            if (retValue.length > 0) retValue = retValue.slice(0, retValue.length - 1);
+        }
+        else if (arr[i] == '--double-next'){
+            if (++i < arr.length) {
+                retValue.push(arr[i]);
+                retValue.push(arr[i]);
+            }
+        }
+        else if (arr[i] == '--double-prev'){
+            if (i > 0) retValue.push(arr[i-1]);
+        }
+        else retValue.push(arr[i]);
+    }
+    return retValue;
 };
