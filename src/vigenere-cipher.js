bf7b7dd47472ecc384934d22095c5c9d07eb8a30
toxAndreev <36535854+toxAndreev@users.noreply.github.com>
2020-03-10 21:19:33 +0200

    Update vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,40 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(mode=true){
+        if (mode === undefined) throw new Error();
+        this.mode = mode;
+        this.alphabetArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+    }
+    encrypt(msg, key) {
+        if (msg === undefined || key === undefined) throw new Error();
+        let keyArray = key.toUpperCase().split('');
+        let keyPosition = 0;
+        let retValue = msg.toUpperCase().split('').map( x => {
+            if (this.alphabetArray.indexOf(x) >= 0){
+                let shift = this.alphabetArray.indexOf(keyArray[keyPosition]);
+                let newIdx = (shift + this.alphabetArray.indexOf(x)) % this.alphabetArray.length;
+                keyPosition = ++keyPosition % keyArray.length;
+                return this.alphabetArray[newIdx];
+            }
+            return x;
+        } );
+        return this.mode ? retValue.join('') : retValue.reverse().join('');
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    decrypt(msg, key) {
+        if (msg === undefined || key === undefined) throw new Error();
+        let keyArray = key.toUpperCase().split('');
+        let keyPosition = 0;
+        let retValue = msg.toUpperCase().split('').map( x => {
+            if (this.alphabetArray.indexOf(x) >= 0){
+                let shift = this.alphabetArray.indexOf(keyArray[keyPosition]);
+                let newIdx = (this.alphabetArray.indexOf(x) - shift);
+                if (newIdx < 0) newIdx += this.alphabetArray.length
+                keyPosition = ++keyPosition % keyArray.length;
+                return this.alphabetArray[newIdx];
+            }
+            return x;
+        } )
+        return this.mode ? retValue.join('') : retValue.reverse().join('');
     }
 }
 
