8b3ef500579e8184b7d46c80acc581a4aeeec840
toxAndreev <36535854+toxAndreev@users.noreply.github.com>
2020-03-10 21:18:05 +0200

    Update recursive-depth.js

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,33 @@
-module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+const chainMaker = {
+  currentChain: [],
+  getLength() {
+    return this.currentChain.length;
+  },
+  addLink(value) {
+    this.currentChain.push(value === undefined ? '' : String(value));
+    return this;
+  },
+  removeLink(position) {
+    try{
+      if (isNaN(position)
+        || position < 1 
+        || position > this.currentChain.length
+        || Math.round(position) != position) throw new Error();
+      this.currentChain.splice(position - 1, 1);
+      return this;
     }
+    catch(ex){
+      this.currentChain = [];
+      throw new Error();
+    }
+  },
+  reverseChain() {
+    this.currentChain.reverse();
+    return this;
+  },
+  finishChain() {
+    let retValue = this.currentChain.map(x => '( ' + x + ' )').join('~~');
+    this.currentChain = [];
+    return retValue;
+  }
 };
