8e2378862a40dab232f173b29c36ff980326234a
toxAndreev <36535854+toxAndreev@users.noreply.github.com>
2020-03-10 21:16:08 +0200

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,12 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let retValue = false;
+  if (members && members instanceof Array){
+    retValue = members.reduce((accum, curVal) => {
+        if (curVal && typeof curVal == 'string'){
+          accum.push(curVal.trim()[0].toUpperCase());
+        }
+        return accum;
+      }, []).sort().join('');
+  }
+  return retValue ? retValue : false;
 };
