d7bdbe0f7edb533ab954b3f2368343cadf3f0c7e
toxAndreev <36535854+toxAndreev@users.noreply.github.com>
2020-03-11 16:29:25 +0200

    fix: change code

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,21 +1,5 @@
-module.exports = class DepthCalculator {
-    maxDepth = 0;
-    currentLevel = 0;
-    calculateDepth(arr, isInternalRun) {
-        if (!isInternalRun){
-            this.maxDepth = 0;
-            this.currentLevel = 0;
-        } 
-        if (arr instanceof Array){
-            this.currentLevel += 1;
-            arr.forEach(x => {
-                if (x instanceof Array){
-                     this.calculateDepth(x, true);
-                }
-            });
-            if (this.currentLevel > this.maxDepth) this.maxDepth = this.currentLevel;
-            this.currentLevel -= 1;
-        }
-        return this.maxDepth;
-    }
-};
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    let turns = Math.pow(2, disksNumber);
+    let seconds = turns / (turnsSpeed / 3600);
+    return { turns: turns, seconds: seconds };
+}
